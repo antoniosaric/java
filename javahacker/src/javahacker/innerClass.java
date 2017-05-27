df4be0561b9621b0dfee8390af1c3c36445bcee0
Tony <saric.tony@gmail.com>
2017-05-26 19:52:00 -0700

    adds nested class

---
+++ javahacker/src/javahacker/innerClass.java
@@ -0,0 +1,25 @@
+package javahacker;
+
+public class innerClass {
+	
+	static public class Animal{
+		String Name;
+		String Voice;
+	}
+	
+	
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		Animal animal = new Animal();
+		animal.Name="Monkey";
+		animal.Voice="hehe";
+
+		
+	}
+	
+	void call(){
+
+	}
+
+}
