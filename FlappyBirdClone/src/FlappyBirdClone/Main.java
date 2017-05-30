77e7d2fbbe6f6ff53bd4cb77d91da96112b5c4a7
Tony <saric.tony@gmail.com>
2017-05-29 20:55:28 -0700

    adds basic game engine

---
+++ FlappyBirdClone/src/FlappyBirdClone/Main.java
@@ -0,0 +1,17 @@
+package FlappyBirdClone;
+
+public class Main {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		Game g = new Game();
+		
+		// initialise game objects
+				
+		// add updatables
+		
+		g.start();
+
+	}
+
+}
